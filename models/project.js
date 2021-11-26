28b4127fdbeb54fd1bc50df79e23a0c237b08c85
unknown <kareldrnec@gmail.com>
2021-11-26 12:37:54 +0100

    pridan model project

---
+++ models/project.js
@@ -0,0 +1,29 @@
+// Schema of project
+
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var projectSchema = new Schema({
+    projectName: {
+        type: String,
+        required: true
+    },
+    projectType: {
+        type: String,
+        required: true
+    }, 
+    projectInfo: {
+        type: String,
+        default: ""
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }, 
+    userId: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Project', projectSchema);
