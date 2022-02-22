104e99cc76437f758893211a19c3936b280c6a77
unknown <kareldrnec@gmail.com>
2022-02-22 18:13:57 +0100

    update

--- public/javascripts/initGraph.js
+++ public/javascripts/initGraph.js
@@ -8,10 +8,10 @@ var selectedStates = [];
 function init(states, activities, projectT, translations) {
     _statesData = states.replace(/&quot;/g, '"');
     _statesData = JSON.parse(_statesData);
-    
+
     _activitiesData = activities.replace(/&quot;/g, '"');
     _activitiesData = JSON.parse(_activitiesData);
-    
+
     projectType = projectT;
 
     _translationsData = translations.replace(/&quot;/g, '"');
@@ -90,7 +90,7 @@ function graphInit() {
                 contextMenu: $(go.Adornment, "Vertical",
                     $("ContextMenuButton",
                         $(go.TextBlock, _translationsData[0]), { click: addActivity }),
-                        // Add Activity
+                    // Add Activity
                     $("ContextMenuButton",
                         $(go.TextBlock, _translationsData[1]), {
                             click: function() {
@@ -157,36 +157,33 @@ function graphInit() {
     myDiagram.model = new go.GraphLinksModel(statesArray, activitiesArray);
 
     myDiagram.add(
-		$(go.Node, "Auto",
-			$(go.Shape, "Rectangle",  // the border
-				{ fill: grayFill }),
-			$(go.Panel, "Table",
-				$(go.RowColumnDefinition, { column: 1, separatorStroke: "black" }),
-				$(go.RowColumnDefinition, { column: 2, separatorStroke: "black" }),
-				$(go.RowColumnDefinition, { row: 1, separatorStroke: "black", background: grayFill, coversSeparators: true }),
-				$(go.RowColumnDefinition, { row: 2, separatorStroke: "black" }),
-				$(go.TextBlock, _translationsData[6],
-					{ row: 0, column: 0, margin: 5, textAlign: "center" }),
-				$(go.TextBlock, "",
-					{ row: 0, column: 1, margin: 5, textAlign: "center" }),
-				$(go.TextBlock, _translationsData[7],
-					{ row: 0, column: 2, margin: 5, textAlign: "center" }),
+        $(go.Node, "Auto",
+            $(go.Shape, "Rectangle", // the border
+                { fill: grayFill }),
+            $(go.Panel, "Table",
+                $(go.RowColumnDefinition, { column: 1, separatorStroke: "black" }),
+                $(go.RowColumnDefinition, { column: 2, separatorStroke: "black" }),
+                $(go.RowColumnDefinition, { row: 1, separatorStroke: "black", background: grayFill, coversSeparators: true }),
+                $(go.RowColumnDefinition, { row: 2, separatorStroke: "black" }),
+                $(go.TextBlock, _translationsData[6], { row: 0, column: 0, margin: 5, textAlign: "center" }),
+                $(go.TextBlock, "", { row: 0, column: 1, margin: 5, textAlign: "center" }),
+                $(go.TextBlock, _translationsData[7], { row: 0, column: 2, margin: 5, textAlign: "center" }),
 
-				$(go.TextBlock, _translationsData[5],
-					{
-						row: 1, column: 0, columnSpan: 3, margin: 5,
-						textAlign: "center", font: "bold 14px sans-serif"
-					}),
+                $(go.TextBlock, _translationsData[5], {
+                    row: 1,
+                    column: 0,
+                    columnSpan: 3,
+                    margin: 5,
+                    textAlign: "center",
+                    font: "bold 14px sans-serif"
+                }),
 
-				$(go.TextBlock, "",
-					{ row: 2, column: 0, margin: 5, textAlign: "center" }),
-				$(go.TextBlock, _translationsData[4],
-					{ row: 2, column: 1, margin: 5, textAlign: "center" }),
-				$(go.TextBlock, "",
-					{ row: 2, column: 2, margin: 5, textAlign: "center" })
-			)
-		)
-	);
+                $(go.TextBlock, "", { row: 2, column: 0, margin: 5, textAlign: "center" }),
+                $(go.TextBlock, _translationsData[4], { row: 2, column: 1, margin: 5, textAlign: "center" }),
+                $(go.TextBlock, "", { row: 2, column: 2, margin: 5, textAlign: "center" })
+            )
+        )
+    );
 
 
 
@@ -224,14 +221,14 @@ function getLinkDataArray(activities) {
     console.log(activitiesData)
 
     for (var i = 0; i < activitiesData.length; i++) {
-        if(activitiesData[i].activityType == "normal") {
+        if (activitiesData[i].activityType == "normal") {
             linkDataArray.push({
                 "from": activitiesData[i].fromState,
                 "to": activitiesData[i].toState,
                 "text": parseLinkTextData(activitiesData[i].values),
                 "color": "B"
             })
-        } else if(activitiesData[i].activityType == "dummy") {
+        } else if (activitiesData[i].activityType == "dummy") {
             linkDataArray.push({
                 "from": activitiesData[i].fromState,
                 "to": activitiesData[i].toState,
@@ -266,14 +263,14 @@ function editState(e, obj) {
     var nodeData = selectedNode.data;
 
     document.getElementById('editStateForm').setAttribute('name', nodeData.key);
-    
+
     var states = _statesData.states;
     var item = states.find(element => element.ID == nodeData.key);
 
     document.getElementById("editStateName").value = item.stateName;
     document.getElementById("editStateInfo").value = item.description;
 
-    if(item.stateName == "Start" || item.stateName == "Finish") {
+    if (item.stateName == "Start" || item.stateName == "Finish") {
         document.getElementById("editStateName").readOnly = true;
     } else {
         document.getElementById("editStateName").readOnly = false;
@@ -332,19 +329,30 @@ function editActivity(e, obj) {
     var activity = activities.find(element => element.fromState == linkData.from && element.toState == linkData.to);
     var activityValues = activity.values;
 
+    console.log(activity)
+
     document.getElementById('editActivityForm').setAttribute('name', activity.ID);
 
     document.getElementById('editedActivityName').value = activity.activityName;
     document.getElementById('editedActivityType').value = activity.activityType;
     document.getElementById('editedActivityDescription').value = activity.description;
 
-    if(activityValues.length == 1) {
+    if (activityValues.length == 1) {
         document.getElementById('editedActivityLength').value = activityValues[0];
-    } else if(activityValues.length == 3) {
+    } else if (activityValues.length == 3) {
         document.getElementById('editedActivityOptimistic').value = activityValues[0];
         document.getElementById('editedActivityMost').value = activityValues[1];
         document.getElementById('editedActivityPessimistic').value = activityValues[2];
-    } 
+    }
+
+    if (activity.activityType == "dummy" && activityValues.length == 1) {
+        $('#editedActivityLength').attr('disabled', 'disabled');
+    } else if (activity.activityType == "dummy" && activityValues.length == 3) {
+        $('#editedActivityOptimistic').attr('disabled', 'disabled');
+        $('#editedActivityMost').attr('disabled', 'disabled');
+        $('#editedActivityPessimistic').attr('disabled', 'disabled');
+    }
+
 
     $('#editActivityModal').modal('show');
     // asi hotovo
@@ -378,15 +386,9 @@ function addCreatedState(state) {
 }
 
 function editSelectedState(stateID, projectID, stateName, stateInfo) {
-    var states = _statesData.states;
-    var item = {
-        stateName: stateName,
-        projectID: projectID,
-        ID: stateID,
-        description: stateInfo
-    };
-    var foundIndex = states.findIndex(element => element.ID == stateID);
-    states[foundIndex] = item;
+    var foundIndex = _statesData.states.findIndex(element => element.ID == stateID);
+    _statesData.states[foundIndex].stateName = stateName;
+    _statesData.states[foundIndex].description = stateInfo
     reload();
 }
 
@@ -398,18 +400,22 @@ function deleteSelectedState(stateID) {
         states.splice(foundIndex, 1)
         reload();
     }
-    // smazat activity napojene na stavy
+    console.log("Vypis")
+    console.log(_activitiesData)
+    console.log("////////")
+        // smazat activity napojene na stavy
 }
 
 function editSelectedActivity(activityID, activityName, activityType, activityDescription, activityValues) {
-    // TODO
-    
+    var foundIndex = _activitiesData.activities.findIndex(element => element.ID == activityID);
+    _activitiesData.activities[foundIndex].activityName = activityName;
+    _activitiesData.activities[foundIndex].activityType = activityType;
+    _activitiesData.activities[foundIndex].description = activityDescription;
+    _activitiesData.activities[foundIndex].values = activityValues;
+    reload();
 }
 
-
-
-
-function deleteSelectedActivity(activityID){
+function deleteSelectedActivity(activityID) {
     var activities = _activitiesData.activities;
     var foundIndex = activities.findIndex(element => element.ID == activityID);
     if (foundIndex > -1) {
@@ -417,8 +423,6 @@ function deleteSelectedActivity(activityID){
         reload();
     }
 }
-
-
 
 function addCreatedActivity(activity) {
     // add created activity to diagram
@@ -436,6 +440,7 @@ function addCreatedActivity(activity) {
     })
     reload();
 }
+
 
 function reload() {
     statesArray = getNodeDataArray(_statesData);
