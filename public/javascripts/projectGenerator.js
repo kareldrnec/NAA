9c5c2e70ef721d26dfeb98bc290c5330dbdb89be
kareldrnec <kareldrnec@gmail.com>
2022-03-22 22:48:32 +0100

    priprava generatoru

--- public/javascripts/projectGenerator.js
+++ public/javascripts/projectGenerator.js
@@ -1,11 +1,25 @@
 // Generator skript
 
-function generateProject() {
+function generateProject(statesCount) {
     console.log("cau")
+    // generovani stavu
+    var states = generateStates(statesCount);
+
+    console.log("States");
+    console.log(states);
+    console.log("ende");
 }
 
-function generateStates() {
-
+function generateStates(count) {
+    var states = [];
+    // add Start State
+    states.push({"name": "Start"});
+    // add Finish State
+    states.push({"name": "Finish"});
+    for(var i = 0; i < count - 2; i++) {
+        states.push({"name": "S" + (i+1)});
+    }
+    return states;
 }
 
 function generateActivities() {
